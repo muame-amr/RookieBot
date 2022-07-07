f9ef43008e7522480f9dcaf1b49d49a2ac24301a
Muhammad Amiruddin <m.amiruddin27@gmail.com>
2022-07-07 23:23:07 +0800

    Everybody made a mistake

--- classes/client.py
+++ classes/client.py
@@ -1,25 +1,26 @@
 import discord
-from multiprocessing.connection import Client
 
 class Client:
     client = discord.Client()
     def __init__(self, token):
         self.token = token
+        self.prefix = '$'
         
     @client.event
     async def on_ready():
         print(f'We have logged in as {Client.client.user}')
 
     @client.event
-    async def on_message(message):
+    async def on_message(self, message):
+        commands = message.content.split(' ')[0].lower()
         if message.author == Client.client.user:
             return
         
-        if message.content.startswith('$hello'):
-            await message.channel.send('👋 Hello!');
+        if prefix.startswith(self.prefix + "hello"):
+            await message.channel.send("Hello! :wave:");
 
-        if message.content.startswith('$ping'):
-            await message.channel.send('🏓 Pong!');
+        if prefix.startswith(self.prefix + "ping"):
+            await message.channel.send("Pong! :ping_pong:");
     
     def run(self):
         Client.client.run(self.token)
